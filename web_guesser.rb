3b93034ab6f125bf03193a78a3f15c89183cf6ca
Addler Pierre-Louis <jhunterswagg365@gmail.com>
2013-07-09 16:50:09 -0400

    This seems to be working well with the way too settings. Also end of the say tuesday

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = (1 + rand(99)).to_s
+secret = (1 + rand(99)).to_s
 
 
 get '/' do
-	erb :index, :locals => {:number => x}
+
+	if secret.to_i + 5 < params["guess"].to_i 
+		guess = "Way TOO HIGH"
+	elsif secret.to_i - 5 > params["guess"].to_i 
+		guess = "Way TOO Low"
+	elsif secret.to_i > params["guess"].to_i
+		guess = "TOO Low"
+	elsif secret.to_i < params["guess"].to_i
+		guess = "Too HIGH"
+	elsif secret.to_i == params["guess"].to_i
+		guess = "You got it!"
+	end
+
+	erb :index, :locals => {:number => secret, :guess => guess}
 end
