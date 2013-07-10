b956b2f373d9a388738e0eccb60979f9d718aaa1
Addler Pierre-Louis <jhunterswagg365@gmail.com>
2013-07-10 11:28:26 -0400

    This is with I3 complete.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,10 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret = (1 + rand(99)).to_s
-
+puts secret
 
 get '/' do
-
+	final_guess = false
 	if secret.to_i + 5 < params["guess"].to_i 
 		guess = "Way TOO HIGH"
 	elsif secret.to_i - 5 > params["guess"].to_i 
@@ -16,7 +16,8 @@ get '/' do
 		guess = "Too HIGH"
 	elsif secret.to_i == params["guess"].to_i
 		guess = "You got it!"
+		final_guess = true
 	end
 
-	erb :index, :locals => {:number => secret, :guess => guess}
+	erb :index, :locals => {:number => secret, :guess => guess, :correct => final_guess}
 end
